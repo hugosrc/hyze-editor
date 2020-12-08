82cac4480a247e2cbae27cc9dec81572e7a5bb8d
hugosrc <hugo.uibai54@gmail.com>
2020-12-08 19:34:58 -0300

    feat: moves terminal-related functions to the terminal.h file

---
+++ include/terminal.h
@@ -0,0 +1,16 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+void die(const char *s);
+
+void disableRawMode();
+
+void enableRawMode();
+
+int editorReadKey();
+
+int getCursorPosition(int *rows, int *cols);
+
+int getWindowSize(int *rows, int *cols);
+
+#endif
