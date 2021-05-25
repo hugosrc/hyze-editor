476efaf2db4b21720c5f4a3c1c8b045f92bb142b
hugosrc <hugo.uibai54@gmail.com>
2021-05-25 00:51:38 -0300

    docs: add documentation to the declarations in the buffer.h file

--- include/buffer.h
+++ include/buffer.h
@@ -4,15 +4,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Constant used to start an empty append buffer.
+*/
 #define ABUF_INIT { NULL, 0 }
 
+/**
+ * The structure of an append buffer is a memory 
+ * pointer to a buffer and its length.
+*/
 struct abuf {
+  /**
+   * memory pointer to a buffer
+  */
   char* b;
+  /**
+   * buffer length
+  */
   int length;
 };
 
+/**
+ * Append a string to an abuf, for which the buffer 
+ * memory block is reallocated, freeing up enough 
+ * memory to append the new string.
+ * 
+ * @param ab append buffer to which the new string will be attached
+ * @param s string to be attached.
+ * @param length length of the string to be attached.
+*/
 void abAppend(struct abuf* ab, const char* s, int length);
 
+/**
+ * It is a destructor that deallocates the dynamic memory 
+ * used by an append buffer.
+ * 
+ * @param ab append buffer which will be deallocated the memory.
+*/
 void abFree(struct abuf* ab);
 
 #endif
