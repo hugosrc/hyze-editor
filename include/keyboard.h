9967edcf654f6f9c1e584595018145873ae8636b
hugosrc <hugo.uibai54@gmail.com>
2021-05-25 10:19:28 -0300

    docs: add documentation to the declarations in the keyboard.h file

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,8 +1,17 @@
 #ifndef KEYBOARD_H_
 #define KEYBOARD_H_
 
+/**
+ * Constant that returns a decimal value for a 
+ * sequence formed by pressing ctrl + key.
+ * 
+ * @param k key (char)
+*/
 #define CTRL_KEY(k) ((k) & 0x1f) 
 
+/**
+ * Custom keys on the keyboard.
+*/
 enum editorKey {
   BACKSPACE = 127, 
   ARROW_LEFT = 1000,
